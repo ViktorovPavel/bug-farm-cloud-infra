1946e917ae8d6a0422af803d54d3b25194602628
Pavel Viktorov <antiplus15@gmail.com>
2026-08-17 14:04:30 +0300

    fix

--- platforms/gcp/backend.tf
+++ platforms/gcp/backend.tf
@@ -1,23 +1,21 @@
 # Фиксируем минимальную версию Terraform и необходимые провайдеры
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.15.8"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 5.0"
+      version = "~> 7.44.0"
     }
-    # Официальный провайдер Talos для автогенерации URL к образу из Image Factory
     talos = {
       source  = "siderolabs/talos"
-      version = "~> 0.7"
+      version = "~> 0.12.0"
     }
   }
 
-  # Удаленный backend для хранения состояния инфраструктуры GCP.
   backend "gcs" {
-    bucket = "bug-farm-tfstate-gcp" # Уникальное имя бакета в GCP
-    prefix = "platforms/gcp"        # Путь к файлу состояния внутри бакета
+    bucket = "bug-farm-tf-state"
+    prefix = "terraform/state"
   }
 }
 
